fe39a25a196d7f10e15075bb23354f34b2ea2712
lumen019 <237933659+lumen019@users.noreply.github.com>
2026-01-01 13:59:15 -0700

    barely function

--- scripts.js
+++ scripts.js
@@ -1,43 +1,94 @@
-function add (arr) { 
-    return arr.reduce((acc, num) => acc + num);
-};
+let firstValue = [];
+let operation;
+let secondValue = [];
+let mode = "first";
 
-function subtract (arr){
-    return arr.reduce((acc, num) => acc - num);
-};
+//math functions
+function add (a, b) { 
+    return a + b; 
+}
 
-function multiply (arr) { 
-   return arr.reduce((acc, num) => acc * num);
-};
+function subtract (a, b){
+    return a - b; 
+}
 
-function divide (arr) {
-    return arr.reduce((acc, num) => acc / num);
-};
+function multiply (a, b) { 
+   return a * b; 
+}
 
-let num1;
-let operator;
-let num2;
+function divide (a, b) {
+    return a / b; 
+}
 
-function operate (operate, num1, num2) {
-    if (operate === "+") {
-        add(num1, num2);
-    } else if (operate === "-") {
-        subtract(num1, num2);
-    } else if (operate === "/") {
-        divide(num1, num2);
-    } else 
-        multiply(num1, num2);
+
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2)
+        case "x":
+            return multiply(num1, num2)
+        case "÷":
+            return divide(num1, num2)
     }
+}
 
-const buttons = document.querySelectorAll("button")
-let display = document.querySelector("#display")
+const btnContainer = document.querySelector("#btnContainer")
+const display = document.querySelector("#display")
 
-buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        display.value += e.target.innerText
-        num1 = e.target.innerText
-    });
+
+function updateDisplay (value) {
+    display.value = value;
+}
+
+function ifNumber (num) {
+    if (mode === "first") {
+        firstValue.push(num);
+        updateDisplay(firstValue.join(""))
+    } else if (mode === "second") {
+        secondValue.push(num);
+        updateDisplay(secondValue.join(""))
+    }
+}
+
+function ifOperator (value) {
+    operation = value;
+    mode = "second"
+}
+
+function ifEqual () {
+    let num1 = Number(firstValue.join(""))
+    let num2 = Number(secondValue.join(""))
+    let result = operate(operation, num1, num2);
+    updateDisplay(result);
+}
+
+function ifClear () {
+    firstValue = [];
+    operation = "";
+    secondValue = [];
+    mode = "first";
+    updateDisplay("");
+}
+
+
+btnContainer.addEventListener ("click", (e) => {
+    const btn = e.target;
+
+    if (btn.classList.contains("number")) {
+        ifNumber(btn.innerText)
+    } else if (btn.classList.contains("operator")) {
+        ifOperator(btn.innerText)
+    } else if (btn.id === "clearBtn") {
+        ifClear()
+    } else if (btn.id === "equalBtn") {
+        ifEqual()
+    }
 })
+
+
+
 
 
 
